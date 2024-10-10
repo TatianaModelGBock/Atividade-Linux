4fc07a3cff2b78acfcdcbcc3d10e4fd6dce5dbbc
tatiana1990modelbock <tatianbock08@gmail.com>
2024-10-10 15:47:36 -0300

    nginx rodando

--- nginx_script.sh
+++ nginx_script.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 # Definir o diretório de saída para armazenar os resultados
@@ -17,9 +18,30 @@ then
     MENSAGEM="O serviço $SERVICO está ativo."
     # Criar arquivo de saída para serviço online
     echo "$DATA_HORA - Serviço: $SERVICO - Status: $STATUS - $MENSAGEM" >> "$DIR_SAIDA/servico_online.log"
+    echo "<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset='utf-8'/>
+  <title>Welcome to nginx!</title>
+ </head>
+ <body>
+   <h2>"$DATA_HORA - Serviço: $SERVICO - Status: $STATUS - $MENSAGEM"</h2>
+ </body>
+</html>" > /var/www/html/index.html
 else
     STATUS="OFFLINE"
     MENSAGEM="O serviço $SERVICO está inativo."
     # Criar arquivo de saída para serviço offline
-    echo "$DATA_HORA - Serviço: $SERVICO - Status: $STATUS - $MENSAGEM" >> "$DIR_SAIDA/servico_offline.log"
+ echo "<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset='utf-8'/>
+  <title>Welcome to nginx!</title>
+ </head>
+ <body>
+   <h2>"$DATA_HORA - Serviço: $SERVICO - Status: $STATUS - $MENSAGEM"</h2>
+ </body>
+</html>" > /var/www/html/index.html
+   
+ echo "$DATA_HORA - Serviço: $SERVICO - Status: $STATUS - $MENSAGEM" >> "$DIR_SAIDA/servico_offline.log"
 fi
